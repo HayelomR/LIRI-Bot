61b36a749e58b595c0eb9c40b6a70bfc1340c2bb
HayelomR <rhayelom@gmail.com>
2017-07-14 15:09:01 -0600

    some changes

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '1eHnqeQEPlCyPZ7hbzRKZRjBu',
+  consumer_secret: 'CHzdYB8gq2oC5zIpA2rESsDAI1DZ9JzfVdzoIQS6tD4BFSx3M9',
+  access_token_key: '884882205926764549-PW28DM8UZ3feubFbvDpgeeNX4s4Z9Xs',
+  access_token_secret: 'UDYOT9eE189KRLSNFc8L4p8a6VGB0ib0yt4ls8yohn20M',
+}
